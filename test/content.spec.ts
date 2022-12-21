6a99b5d2c7a390d31f534c1d03427fe9bd6baa2e
cxavier6 <cxavier.reis@gmail.com>
2022-12-21 20:23:17 -0300

    test: adiciona testes de tamanho de caracteres

--- test/content.spec.ts
+++ test/content.spec.ts
@@ -1,6 +1,17 @@
 import { Content } from "../src/app/entities/content"
 
-test('it should be able to create a notification content', () => {
-    const content = new Content ('Você recebeu uma solicitação')
-    expect(content).toBeTruthy()
+describe('Notification content', () => {
+    test('it should be able to create a notification content', () => {
+        const content = new Content ('Você recebeu uma solicitação')
+        expect(content).toBeTruthy()
+    })
+    
+    test('it should not be able to create a notification content with less than 5 characters', () => {
+        expect(() => new Content('aaa')).toThrowError()
+    })
+    
+    test('it should not be able to create a notification content with less than 5 characters', () => {
+        expect(() => new Content('a'.repeat(241))).toThrowError()
+    })
 })
+
