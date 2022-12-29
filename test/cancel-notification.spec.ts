c308464152128355c81286040f92bafad0667dce
cxavier6 <cxavier.reis@gmail.com>
2022-12-29 11:23:59 -0300

    test: adiciona testes de cancel notification

---
+++ test/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from "@app/entities/content"
+import { Notification } from "@app/entities/notification"
+import { CancelNotification } from "@app/use-cases/cancel-notification"
+import { NotificationNotFound } from "@app/use-cases/errors/notification-not-found"
+import { InMemoryNotificationsRepository } from "./repositories/in-memory-notifications-repository"
+
+
+
+describe('Cancel notification', () => {
+    test('it should be able to send a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository()
+        const cancelNotification = new CancelNotification(notificationsRepository)
+
+        const notification = new Notification({
+            category: 'social',
+            content: new Content('Nova solicitação'),
+            recipientId: 'example-recipient-id',
+        })
+
+        await notificationsRepository.create(notification)
+
+        await cancelNotification.execute({
+            notificationId: notification.id,
+        })
+
+        expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date))
+    })
+
+    test('it should not be able to cancel a non existing notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository()
+        const cancelNotification = new CancelNotification(notificationsRepository)
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-notification-id',
+            })
+        }).rejects.toThrow(NotificationNotFound)
+    })
+})
