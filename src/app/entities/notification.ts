6b0e61a3c37a5be0fa38d581578ddb5c8668f985
cxavier6 <cxavier.reis@gmail.com>
2022-12-29 11:21:23 -0300

    feat: add interface, getter e setter de canceledAt

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -3,16 +3,17 @@ import { Replace } from "src/helpers/Replace";
 import { Content } from "./content";
 
 export interface NotificationProps {
-    recipientId: string;
-    content: Content;
-    category: string;
-    readAt?: Date | null;
-    createdAt: Date;
+    recipientId: string
+    content: Content
+    category: string
+    readAt?: Date | null
+    canceledAt?: Date | null
+    createdAt: Date
 }
 
 export class Notification {
-    private _id: string;
-    private props: NotificationProps;
+    private _id: string
+    private props: NotificationProps
 
     constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
         this._id = randomUUID()
@@ -27,7 +28,7 @@ export class Notification {
     }
 
     public set recipientId(recipientId: string) {
-        this.props.recipientId = recipientId;
+        this.props.recipientId = recipientId
     }
 
     public get recipientId(): string {
@@ -35,7 +36,7 @@ export class Notification {
     }
 
     public set content(content: Content) {
-        this.props.content = content;
+        this.props.content = content
     }
 
     public get content(): Content {
@@ -43,7 +44,7 @@ export class Notification {
     }
 
     public set category(category: string) {
-        this.props.category = category;
+        this.props.category = category
     }
 
     public get category(): string {
@@ -51,11 +52,19 @@ export class Notification {
     }
 
     public set readAt(readAt: Date | null | undefined) {
-        this.props.readAt = readAt;
+        this.props.readAt = readAt
     }
 
     public get readAt(): Date | null | undefined {
         return this.props.readAt
+    }
+
+    public cancel() {
+        this.props.canceledAt = new Date()
+    }
+
+    public get canceledAt(): Date | null | undefined {
+        return this.props.canceledAt
     }
 
     public get createdAt(): Date {
