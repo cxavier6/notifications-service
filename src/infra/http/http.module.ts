007204265df83ecb8638e3eac21148da2c93f7e3
cxavier6 <cxavier.reis@gmail.com>
2023-01-04 14:30:05 -0300

    feat: adiciona os providers dos usecases

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,11 +2,24 @@ import { Module } from "@nestjs/common";
 import { SendNotification } from "@app/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
 import { NotificationsController } from "./controllers/notifications.controller";
+import { CancelNotification } from "@app/use-cases/cancel-notification";
+import { GetRecipientNotifications } from "@app/use-cases/get-recipient-notifications";
+import { ReadNotification } from "@app/use-cases/read-notification";
+import { UnreadNotification } from "@app/use-cases/unread-notification";
+import { CountRecipientNotification } from "@app/use-cases/count-recipient-notifications";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification]
+    providers: [
+        SendNotification,
+        CancelNotification,
+        GetRecipientNotifications,
+        ReadNotification,
+        UnreadNotification,
+        CountRecipientNotification,
+
+    ]
 })
 
 export class HttpModule{}
